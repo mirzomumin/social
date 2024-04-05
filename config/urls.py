39c1c3d2e1ec5e7e1136ff5ef0d64d641ee5ef3e
Mirzomumin <mirzomumin@list.ru>
2024-04-05 17:58:52 +0400

    Run development server with HTTPS protocol

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
+    path("social-auth/", include("social_django.urls", namespace="social")),
 ]
 
 if settings.DEBUG:
