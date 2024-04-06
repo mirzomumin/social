6829e2eca6649375ab0a159d8d81a824ca465b35
Mirzomumin <mirzomumin@list.ru>
2024-04-06 04:44:50 +0400

    Add login with google account

--- config/settings.py
+++ config/settings.py
@@ -153,4 +153,26 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "account.authentication.EmailAuthBackend",
+    "social_core.backends.google.GoogleOAuth2",
+]
+
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ[
+    "SOCIAL_AUTH_GOOGLE_OAUTH2_KEY"
+]  # Google Client ID
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ[
+    "SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET"
+]  # Google Client Secret
+
+SOCIAL_AUTH_PIPELINE = [
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "account.authentication.create_profile",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
 ]
